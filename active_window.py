7137aa9fbe2facf6ddede176983092004f3cc793
Gabriele Farei <JayFarei@users.noreply.github.com>
2019-12-29 10:55:33 +0100

    one output vs data stream

--- active_window.py
+++ active_window.py
@@ -2,13 +2,10 @@ from AppKit import NSWorkspace
 import time
 
 active_window_name = ""
+new_window_name = (NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName'])
+active_window_name = new_window_name.encode(encoding="utf-8", errors="strict")
+print (active_window_name)
 
-while True:
-    new_window_name = (NSWorkspace.sharedWorkspace()
-    .activeApplication()['NSApplicationName'])
+# to be used in ubericht - the output has to be unique and triggered by the command vs a stream of data.
 
-    if active_window_name != new_window_name:
-        active_window_name = new_window_name.encode(encoding="utf-8", errors="strict")
-        print (active_window_name)
-
-# If needed for performance reasons time.sleep (10)
+## only works with python 2.7
