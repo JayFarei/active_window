97154f540dea8190addd5eae057e362b5f9822bb
Gabriele Farei <JayFarei@users.noreply.github.com>
2019-12-29 17:55:03 +0100

    docs

--- active_window.py
+++ active_window.py
@@ -6,6 +6,6 @@ new_window_name = (NSWorkspace.sharedWorkspace().activeApplication()['NSApplicat
 active_window_name = new_window_name.encode(encoding="utf-8", errors="strict")
 print (active_window_name)
 
-# to be used in ubericht - the output has to be unique and triggered by the command vs a stream of data.
+# to be used in ubersicht - the output has to be unique and triggered by the command vs a stream of data.
 
 ## only works with python 2.7
